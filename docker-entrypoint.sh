a7867b00ddd39e3ce7b9d1441f74cb33ecdeb749
dragonflylee <dragonflylee@outlook.com>
2019-12-11 04:20:47 +0000

    Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+[ $DEBUG ] && set -x
+
+if [ `ls -A /app/zentaopms | wc -w` = 0 ]; then
+  cp -a /var/www/zentaopms/* /app/zentaopms
+fi
+
+if [ "`cat /app/zentaopms/VERSION`" != "`cat /var/www/zentaopms/VERSION`" ]; then
+  cp -a /var/www/zentaopms/* /app/zentaopms
+fi
+
+chmod -R 777 /app/zentaopms/www/data
+chmod -R 777 /app/zentaopms/tmp
+chmod 777 /app/zentaopms/www
+chmod 777 /app/zentaopms/config
+chmod -R a+rx /app/zentaopms/bin/*
+
+/etc/init.d/apache2 start
+
+chown -R www-data:www-data /app/zentaopms
+chown -R mysql:mysql /var/lib/mysql/
+if [ `ls -A /var/lib/mysql/ | wc -w` = 0 ]; then
+  mysql_install_db --defaults-file=/etc/mysql/my.cnf
+  /etc/init.d/mysql start
+  /usr/bin/mysqladmin -uroot password $MYSQL_ROOT_PASSWORD
+  mysql -uroot -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE user='root';FLUSH PRIVILEGES;"
+else
+  /etc/init.d/mysql start
+fi
+
+if [ `ls -A /app/zentaopms/config/ | wc -w` != 0 ]; then
+  rm -f /app/zentaopms/www/install.php /app/zentaopms/www/upgrade.php
+fi
+
+tail -f /var/log/apache2/zentao_error_log
